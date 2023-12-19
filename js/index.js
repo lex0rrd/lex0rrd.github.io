12b6fb1eaf94db0feaedc72666012f71dbab39d1
Илья <129730610+lex0rrd@users.noreply.github.com>
2023-12-19 21:41:04 +0300

    Create index.js

---
+++ js/index.js
@@ -0,0 +1,33 @@
+// Создаем кнопку "Вход с помощью Apple"
+var appleSignInButton = document.createElement("button");
+appleSignInButton.textContent = "Вход с помощью Apple";
+document.body.appendChild(appleSignInButton);
+
+// Обработчик нажатия на кнопку "Вход с помощью Apple"
+appleSignInButton.addEventListener("click", function() {
+  // Запускаем процесс аутентификации через Apple ID
+  AppleID.auth.init({
+        clientId : 'app.getglam.api',
+        scope : 'email',
+        redirectURI : serverURL,
+        state : 'initial',
+        nonce : 'somenonce',
+        usePopup : true
+  });
+
+  // Запускаем аутентификацию
+  AppleID.auth.signIn();
+});
+
+// Обработчик успешной аутентификации через Apple ID
+AppleID.auth.onSignIn(function (authorization) {
+  // Получаем данные пользователя из успешной аутентификации
+  var user = authorization.user;
+
+  // Используем данные пользователя для создания платежа через Stripe API или для других целей
+  var email = user.email;
+  var user_agent = navigator.userAgent;
+  var ip = "user_ip_address"; // Здесь нужно получить реальный IP пользователя, например, с помощью серверной части вашего приложения
+
+  // Далее вы можете использовать эти данные для создания платежа через Stripe API или для других целей
+});
